0572887b022bfb5f38bb08fdbc6fd11cf14c5748
Rahul <rahulkappala699@gmail.com>
2023-06-06 15:16:47 -0500

    update

--- com/backend/week2/FizzBuzz.java
+++ com/backend/week2/FizzBuzz.java
@@ -5,21 +5,19 @@ import java.util.*;
 
 public class FizzBuzz {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number");
-        int number = sc.nextInt();
-        for(int i=1;i<=number;i++) {
-            if(i%3==0 && i%5==0) {
-                System.out.println("FizzBuzz"+i);
-            }else if(i%5==0) {
-                System.out.println("Fizz"+i);
-            }else if(i%3==0) {
-                System.out.println("Buzz"+i);
-            }else {
-                System.out.println("not a fizz number"+i);
+
+        int number = 100;
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i+" ");
             }
         }
-        sc.close();
-    }
 
+    }
 }
