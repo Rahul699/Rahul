11646168d9c7993acc77ea7b4c0214422ca5302a
Rahul <rahulkappala699@gmail.com>
2023-06-07 15:25:58 -0500

    photo

--- com/backend/week2/Rotate.java
+++ com/backend/week2/Rotate.java
@@ -4,7 +4,7 @@ public class Rotate {
     public static void main(String[] args) {
         int[] y={4,6,12,23,45,56,67};
         int N=y.length;
-        int shift=2;
+        int shift=3;
         for(int i=0;i<N;i++) {
             if (i < N - shift) {
                 System.out.println(y[i + shift]);
