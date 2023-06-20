16a0189c3e0abe364331c69f12b7d6b74cd525aa
Rahul <rahulkappala699@gmail.com>
2023-06-20 14:23:01 -0500

    new

---
+++ com/backend/week3/Vowel.java
@@ -0,0 +1,24 @@
+package com.backend.week3;
+
+import java.util.Arrays;
+
+public class Vowel {
+
+    public static void main(String[] args) {
+        String[] strings = {"rahul", "kumar", "sai", "teja"};
+
+        Arrays.stream(strings)
+                .filter(str -> containsVowels(str))
+                .forEach(str -> System.out.println(str + " - " + countVowels(str)));
+    }
+
+    public static boolean containsVowels(String str) {
+        return str.toLowerCase().matches(".*[aeiou].*");
+    }
+
+    public static int countVowels(String str) {
+        return (int) str.toLowerCase().chars()
+                .filter(ch -> "aeiou".indexOf(ch) != -1)
+                .count();
+    }
+}
