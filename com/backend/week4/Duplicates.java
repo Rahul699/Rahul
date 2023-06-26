9aec05b487bc05bb0a968d644d9f1b4fe2b551de
Rahul <rahulkappala699@gmail.com>
2023-06-26 15:10:38 -0500

    new

---
+++ com/backend/week4/Duplicates.java
@@ -0,0 +1,28 @@
+package com.backend.week4;
+
+import java.util.LinkedHashSet;
+
+public class Duplicates {
+    public static String removeDuplicates(String str) {
+        LinkedHashSet<Character> charSet = new LinkedHashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            charSet.add(ch);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Character ch : charSet) {
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String input = "sai Kiran";
+        String result = removeDuplicates(input);
+        System.out.println(result);
+    }
+
+
+}
